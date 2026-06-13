f559bc630e283678aeaaa3b47beffeadfd119596
yaggen <jack@jackhack.me>
2026-06-14 01:31:05 +0200

    added per VM IP-address

--- terraform/environments/lab/main.tf
+++ terraform/environments/lab/main.tf
@@ -1,22 +1,24 @@
 locals {
   vms = {
     vm1 = {
-      name = "backup"
-      node = "labbet03"
-      memory = 4096
-      cores = 2
+      name      = "backup"
+      node      = "labbet03"
+      memory    = 4096
+      cores     = 2
+      ipconfig0 = "ip=192.168.30.25/24,gw=192.168.30.1"
     }
   }
 }
 
 module "vms" {
-  source = "../../modules/vm"
+  source   = "../../modules/vm"
   for_each = local.vms
 
   name        = each.value.name
   target_node = each.value.node
   memory      = each.value.memory
   cores       = each.value.cores
+  ipconfig0   = try(each.value.ipconfig0, "ip=dhcp")
   template    = "debian-13-cloud-template"
 
   ci_user        = "yag"
